1362b4bc2910621541bdfdc6764ba5f3f66df7e0
clarkb21 <bmclark7694@gmail.com>
2022-09-15 10:17:19 -0400

    Adding Deliverable 3 files

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -14,3 +14,12 @@ suspension_coil_data <- read.csv("Suspension_Coil.csv", stringsAsFactors = F, ch
 total_summary <-  suspension_coil_data %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), Standard_Deviation=sd(PSI), .groups='keep')
 #use group by to determine summary statistics for each lot
 lot_summary <-  suspension_coil_data %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), Standard_Deviation=sd(PSI), .groups='keep')
+
+#Deliverable 3
+#perform t-test to determine if lots are statistically different from the population mean of 1500 PSI
+t.test(x=(suspension_coil_data$PSI), mu=1500, alternative = "two.sided")
+
+#Perform t-test with subsets for the three different manufacturing lots
+lot1_test <- t.test(subset(suspension_coil_data, Manufacturing_Lot =="Lot1")$PSI, mu=1500)
+lot2_test <- t.test(subset(suspension_coil_data, Manufacturing_Lot =="Lot2")$PSI, mu=1500)
+lot3_test <- t.test(subset(suspension_coil_data, Manufacturing_Lot =="Lot3")$PSI, mu=1500)
